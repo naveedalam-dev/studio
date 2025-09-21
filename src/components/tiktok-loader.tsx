85b243c2cae6f90f6fb5022688c5d7d950336904
Jawad Khan <bdfpcompany@gmail.com>
2025-09-21 01:08:25 +0000

    the implemented a TikTok-style loading animation is looking blur and sma

--- src/components/tiktok-loader.tsx
+++ src/components/tiktok-loader.tsx
@@ -2,9 +2,9 @@ import { cn } from "@/lib/utils";
 
 export function TikTokLoader({ className }: { className?: string }) {
   return (
-    <div className={cn("relative flex h-20 w-20 items-center justify-center", className)}>
-      <div className="absolute h-8 w-8 rounded-full tiktok-loader-dot-1"></div>
-      <div className="absolute h-8 w-8 rounded-full tiktok-loader-dot-2"></div>
+    <div className={cn("relative flex h-24 w-24 items-center justify-center", className)}>
+      <div className="absolute h-10 w-10 rounded-full tiktok-loader-dot-1"></div>
+      <div className="absolute h-10 w-10 rounded-full tiktok-loader-dot-2"></div>
     </div>
   );
 }
